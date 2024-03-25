dfd3b8498881e74fc792592d675dd7c228fd1125
zedchichi <93988541+zedchichi@users.noreply.github.com>
2024-03-25 20:46:41 +0000

    "Register views blueprint in app's create\_app function and update main.py to use create\_app"
    initialised flask properly

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from .views import app as views_blueprint
 
 def create_app():
     app = Flask(__name__)
     # Configure your app and register blueprints here
-
+    app.register_blueprint(views_blueprint)
+    
     return app
--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from app import create_app
 from flask import Flask
 
 
-# app = Flask(__name__)
-
-# if __name__ == '__main__':
-#     from  app import *
-#     app.run(debug=True)
-
 app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
