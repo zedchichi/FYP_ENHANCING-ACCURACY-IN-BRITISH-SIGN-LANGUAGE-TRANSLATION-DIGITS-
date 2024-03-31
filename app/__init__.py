c945b274e61ce6e1aae3ea105a80ec057e62c709
zedchichi <93988541+zedchichi@users.noreply.github.com>
2024-03-31 21:25:34 +0100

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .views import views_blueprint
-
-def create_app():
-    app = Flask(__name__)
-    # Configure your app and register blueprint
-    app.register_blueprint(views_blueprint)
-    
-    return app
