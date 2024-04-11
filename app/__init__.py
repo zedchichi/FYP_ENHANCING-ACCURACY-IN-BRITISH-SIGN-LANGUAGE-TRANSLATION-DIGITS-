8e6e33abe2c6ee6a5cd3f2183fc1cff090d1a16f
zedchichi <93988541+zedchichi@users.noreply.github.com>
2024-04-11 22:11:39 +0100

    Update __init__.py
    moved app.config into __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
-from .views import views_blueprint
 
 def create_app():
-    app = Flask(__name__)
-    # Configure your app and register blueprint
+    app = Flask(__name__, static_folder='static')
+
+    from app.views import views_blueprint
     app.register_blueprint(views_blueprint)
-    
+
+    app.config['UPLOAD_FOLDER'] = 'uploads/'
+    app.config['CAPTURED_FOLDER'] = 'capture/'
+    app.config['CLASSIFIED_FOLDER'] = 'classified'
+
     return app
