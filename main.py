160cf4348a1c3f2195ff87a92fa69fcd2c69c057
zedchichi <93988541+zedchichi@users.noreply.github.com>
2024-04-13 00:40:05 +0100

    Update main.py
    applied a schedule for retraining
    using from apscheduler.schedulers.background import BackgroundScheduler

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 from app import create_app
-from flask import Flask
-
+import atexit
 
 app = create_app()
 
+def schedule_retraining():
+    from app.views import retrain_models
+    retrain_models()
+
 if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=schedule_retraining, trigger="interval", days=1)  # Retrains every day; adjust as necessary
+    scheduler.start()
+
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
+
     app.run(debug=True)
+
