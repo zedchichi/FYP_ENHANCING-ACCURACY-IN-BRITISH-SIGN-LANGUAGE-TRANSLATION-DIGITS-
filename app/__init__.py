ba1e1a946badd5dcf8ffd760025df0a87eecbc0b
zedchichi <93988541+zedchichi@users.noreply.github.com>
2024-04-13 00:46:26 +0100

    Update __init__.py
    Using application context properly to load model after the application has been created and in the appropriate context
    
    also had problems with views trying to access model paths before they have been set, so had to use initialise_model to load model when app starts after defining paths

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,23 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, static_folder='static')
+    app = Flask(__name__)
 
-    from app.views import views_blueprint
-    app.register_blueprint(views_blueprint)
+
+    # from app.views import views_blueprint
+    # app.register_blueprint(views_blueprint)
 
     app.config['UPLOAD_FOLDER'] = 'uploads/'
     app.config['CAPTURED_FOLDER'] = 'capture/'
-    app.config['CLASSIFIED_FOLDER'] = 'classified'
+    app.config['CLASSIFIED_FOLDER'] = 'classified/'
+    app.config['VGG_CLASSIFIED'] = 'classified/vgg/'
+    app.config['MOBILENET_CLASSIFIED'] = 'classified/mobilenet/'
+    app.config['mobilenet_path'] = r'C:\Users\anazi\FYP\app\BSL_MobileNet_HD_build_mobilenet_hyper4.h5'
+    app.config['vggmodel_path'] = r'C:\Users\anazi\FYP\app\BSL_VGG16_Cus_FT_HD_Best_Model3.h5'
+
+    with app.app_context():
+        from app.views import views_blueprint, initialize_models
+        initialize_models()
+        app.register_blueprint(views_blueprint)
 
     return app
